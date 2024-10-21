ff9baba09073d26aae1e18341402c0d33722d7be
WilliamGuo2002 <yg17@ualberta.ca>
2024-10-21 17:17:09 -0600

    added new event dialog(not finished)

--- app/src/main/java/com/example/breeze0events/OrganizerEventActivity.java
+++ app/src/main/java/com/example/breeze0events/OrganizerEventActivity.java
@@ -1,7 +1,13 @@
 package com.example.breeze0events;
 
+import static android.app.PendingIntent.getActivity;
+
+import static java.security.AccessController.getContext;
+
+import android.app.Dialog;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.os.Bundle;
 import android.content.Intent;
@@ -10,23 +16,87 @@ import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import android.widget.Toast;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import java.util.ArrayList;
-
-public class OrganizerEventActivity exteds AppCompatActivity {
+import androidx.fragment.app.DialogFragment;
+/*
+public class OrganizerEventActivity exteds DialogFragment{
     private FirebaseFirestore db;
     private OverallStorageController overallStorageController;
     private ListView eventListView;
     private ArrayAdapter<String> eventListAdapter;
     private ArrayList<String> eventList;
+    private EditText event_name_bar;
+    private EditText event_date_bar;
+    private EditText event_time_bar;
+    private EditText max_entrants_bar;
+    private EditText sighup_due_date_bar;
+    private EditText event_description_bar;
+    private EditText no_of_attendees_bar;
+    private OnFragmentInteractionListener listener;
+    public interface OnFragmentInteractionListener{
+        void onOkPressed(Event newEvent);
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onAttach(@NonNull Context context) {
+        super.wait(context);
+        if (context instanceof OnFragmentInteractionListener){
+            listener = (OnFragmentInteractionListener) context;
+        }
+        else{
+            throw new RuntimeException(context + "need to implement OnFragmentInteractionListener");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@NonNull Bundle savedInstanceState){
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.organizer_event_activity, null);
+        event_name_bar = view.findViewById(R.id.event_name_bar);
+        event_date_bar = view.findViewById(R.id.event_date_bar);
+        event_time_bar = view.findViewById(R.id.event_time_bar);
+        max_entrants_bar = view.findViewById(R.id.max_entrants_bar);
+        sighup_due_date_bar = view.findViewById(R.id.sighup_due_date_bar);
+        event_description_bar = view.findViewById(R.id.event_description_bar);
+        no_of_attendees_bar = view.findViewById(R.id.no_of_attendees_bar);
+
+        final OrganizerMyListActivity current = (OrganizerMyListActivity) getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        return builder.setView(view).setTitle("Create New Event")
+                .setNegativeButton("Cancel",null)
+                //.setNeutralButton("Generate QR")
+                //.setNeutralButton("Upload Poster")
+                .setPositiveButton("Add and Save",(dialoginterface,i) ->{
+                    String name = event_name_bar.getText().toString();
+                    String date = event_date_bar.getText().toString();
+                    String time = event_time_bar.getText().toString();
+                    String max_entrants = max_entrants_bar.getText().toString();
+                    String sighup_due_date = sighup_due_date_bar.getText().toString();
+                    String description = event_description_bar.getText().toString();
+                    String no_of_attendees = no_of_attendees_bar.getText().toString();
+
+                    // check if all fields are filled properly
+
+                }).create();
+
 
     }
+
 }
+*/
+
