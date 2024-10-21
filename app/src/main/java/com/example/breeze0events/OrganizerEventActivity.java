bb55f1d71b28520e6ec18879f30aaca480fc18f1
WilliamGuo2002 <yg17@ualberta.ca>
2024-10-21 16:47:08 -0600

    added event layout

--- app/src/main/java/com/example/breeze0events/OrganizerEventActivity.java
+++ app/src/main/java/com/example/breeze0events/OrganizerEventActivity.java
@@ -1,4 +1,32 @@
 package com.example.breeze0events;
 
-public class OrganizerEventActivity {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import android.widget.Toast;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+public class OrganizerEventActivity exteds AppCompatActivity {
+    private FirebaseFirestore db;
+    private OverallStorageController overallStorageController;
+    private ListView eventListView;
+    private ArrayAdapter<String> eventListAdapter;
+    private ArrayList<String> eventList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
 }
