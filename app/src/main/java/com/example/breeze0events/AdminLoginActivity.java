69e946c9e600f8dfb946314123364eed51768582
stanqiu111 <stanqiu7438@gmail.com>
2024-10-29 17:22:30 -0600

    Remove events and facility
    Create remove events and facility fragment, and update their UI. Connect overall login page to admin welcome page

---
+++ app/src/main/java/com/example/breeze0events/AdminLoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.breeze0events;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminLoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+        Button profile_remove_button=findViewById(R.id.removeButton1);
+        Button event_remove_button=findViewById(R.id.removeButton2);
+        Button facility_button=findViewById(R.id.facility);
+        //by clicking organization profile button
+        profile_remove_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                Intent intent = new Intent(AdminLoginActivity.this, AdminOperateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        event_remove_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminLoginActivity.this, AdminEventActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        facility_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                Intent intent = new Intent(AdminLoginActivity.this, AdminFacilityActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
