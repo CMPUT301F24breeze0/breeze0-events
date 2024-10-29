73953c837ad1c0a6966e019c1d4548f0e8457d11
anyname-ui <shengku2@ualberta.ca>
2024-10-29 14:21:03 -0400

    admin change
    I make some admin main activity which i can go to the organization profile from admin main page

--- app/src/main/java/com/example/breeze0events/AdminOperateActivity.java
+++ app/src/main/java/com/example/breeze0events/AdminOperateActivity.java
@@ -1,4 +1,26 @@
 package com.example.breeze0events;
 
-public class AdminOperateActivity {
-}
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class AdminOperateActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_main_activity);
+        Button organization_button=findViewById(R.id.organization);
+        Button entrant_button=findViewById(R.id.entrant);
+        //by clicking organization profile button
+        organization_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                Intent intent = new Intent(AdminOperateActivity.this, AdminOrganizationProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        }
+    }
+
