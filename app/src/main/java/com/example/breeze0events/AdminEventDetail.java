d64db14501823b4e64b0834a69eddcfa7cd8b1d8
stanqiu111 <stanqiu7438@gmail.com>
2024-11-02 22:01:54 -0600

    Add QR code and update event
    remove events

--- app/src/main/java/com/example/breeze0events/AdminEventDetail.java
+++ app/src/main/java/com/example/breeze0events/AdminEventDetail.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class AdminEventDetail extends AppCompatActivity {
     private OverallStorageController overallStorageController;
@@ -21,6 +24,8 @@ public class AdminEventDetail extends AppCompatActivity {
     private TextView eventDescription;
     Event selected_event;
     private String eventID;
+    private ArrayList<Event> eventList;
+    private ArrayList<String> eventListDisplay;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +33,7 @@ public class AdminEventDetail extends AppCompatActivity {
         setContentView(R.layout.events_detail_fragment);
 
         Button return_button = findViewById(R.id.cancelButton);
+        Button delete_button = findViewById(R.id.Deletebutton);
         eventTitle = findViewById(R.id.EventDetail);
         eventName = findViewById(R.id.EventName);
         eventDate = findViewById(R.id.EventDate);
@@ -35,7 +41,9 @@ public class AdminEventDetail extends AppCompatActivity {
         signUpDueDay = findViewById(R.id.duedate);
         eventDescription = findViewById(R.id.description);
         overallStorageController = new OverallStorageController();
+
         String id = (String)getIntent().getSerializableExtra("selectedID");
+        eventListDisplay = getIntent().getStringArrayListExtra("eventListDisplay");
 
         overallStorageController.getEvent(String.valueOf(id), new EventCallback() {
             @Override
@@ -44,7 +52,7 @@ public class AdminEventDetail extends AppCompatActivity {
                 eventTitle.setText("Event Detail");
                 eventName.setText("Event Name: " + selected_event.getName());
                 eventDate.setText("Event start from " + event.getStartDate() + " - " + event.getEndDate());
-                maxEntrants.setText("Max number of entrants: " + String.valueOf(event.getEntrants().size()));
+                maxEntrants.setText("Max number of entrants: " + event.getEntrants().size());
                 signUpDueDay.setText("Sign-up due: " + /* add due date if available */ "");
                 //eventDescription.setText(event.getDescription());
 
@@ -60,6 +68,35 @@ public class AdminEventDetail extends AppCompatActivity {
         return_button.setOnClickListener(v -> {
             finish();
         });
+
+        delete_button.setOnClickListener(v -> {
+            if (id != null && !id.isEmpty()) {
+                overallStorageController.deleteEvent(id);
+                Log.d("AdminEventDetail", "Delete function called from OverallStorageController");
+                if (eventList != null) {
+                    eventList.removeIf(event -> event.getEventId().equals(id));
+                }
+                if (eventListDisplay  != null) {
+                    eventListDisplay.clear();
+                    for (Event event : eventList) {
+                        String info = "Name: " + event.getName() + "\nStart_date: " + event.getStartDate()
+                                + "\nEnd_date: " + event.getEndDate();
+                        eventListDisplay.add(info);
+                    }
+                }
+
+
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("UPDATED_LIST", eventListDisplay);
+                setResult(RESULT_OK, resultIntent);
+
+                Toast.makeText(this, "Event deleted successfully", Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                Log.e("AdminEventDetail", "Invalid event ID");
+            }
+        });
+
 
     }
 }
