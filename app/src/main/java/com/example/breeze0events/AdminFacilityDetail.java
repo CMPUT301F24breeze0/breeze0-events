2f459f10a162750383e9880d61d7870ad6d8dfc6
stanqiu111 <stanqiu7438@gmail.com>
2024-11-15 16:15:31 -0700

    TA suggestion confirmation message
    Add delete confirmation message and update facility detail

--- app/src/main/java/com/example/breeze0events/AdminFacilityDetail.java
+++ app/src/main/java/com/example/breeze0events/AdminFacilityDetail.java
@@ -1,20 +1,63 @@
 package com.example.breeze0events;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+
 /**
  * AdminFacilityDetail class provides the UI and backend functionality for displaying detailed information
  * about a specific facility within the Admin section of the Breeze0Events application. This class primarily
  * interacts with the OverallStorageController to retrieve facility data and displays it within the UI elements.
  */
+public class AdminFacilityDetail extends AppCompatActivity {
+   private OverallStorageController overallStorageController;
+   private TextView facilityName;
+   private TextView facilityID;
+   private TextView violatedPolices;
+   private TextView evidence;
+   private String facilityId;
+   private ArrayList<Event> facilityList;
+   private ArrayList<String> facilityListDisplay;
 
- public class AdminFacilityDetail {
-    private OverallStorageController overallStorageController;
-    private TextView facilityName;
-    private TextView facilityID;
-    private TextView violatedPolices;
-    private TextView evidence;
-    private String FacilityID;
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.admin_facility_detail);
+
+      Button return_button = findViewById(R.id.cancelButton);
+      Button delete_button = findViewById(R.id.Deletebutton);
+      facilityName = findViewById(R.id.facilityName);
+      facilityID = findViewById(R.id.facilityID);
+
+
+      overallStorageController = new OverallStorageController();
+      facilityId = getIntent().getStringExtra("selectedID");
+      facilityListDisplay = getIntent().getStringArrayListExtra("facilityListDisplay");
+
+
+      overallStorageController.getFacility(String.valueOf(facilityId), new FacilityCallback() {
+         @Override
+         public void onSuccess(Facility facility) {
+            facilityName.setText("Facility Name:"+ facility.getLocation());
+            facilityID.setText("Facility ID:" + facility.getFacilityId());
+         }
+
+         @Override
+         public void onFailure(String errorMessage) {
+            Log.e("AdminFacilityDetail", "Failed to fetch admins: " + errorMessage);
+         }
+      });
+
+      return_button.setOnClickListener(v -> {
+         finish();
+      });
+   }
 }
