ad7fe1080d9236c51e33f104d898b729c804fd2e
anyname-ui <shengku2@ualberta.ca>
2024-11-19 23:02:09 -0500

    admin change
    I add a function which we can delete the image of event

--- app/src/main/java/com/example/breeze0events/AdminQRcode.java
+++ app/src/main/java/com/example/breeze0events/AdminQRcode.java
@@ -3,11 +3,8 @@ package com.example.breeze0events;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -44,7 +41,7 @@ public class AdminQRcode extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.qr_code_fragment);
 
-        backButton = findViewById(R.id.backButton);
+        backButton = findViewById(R.id.back_in_main);
         deleteButton = findViewById(R.id.delete_button);
         qrCodeImageView = findViewById(R.id.QRcode);
 
