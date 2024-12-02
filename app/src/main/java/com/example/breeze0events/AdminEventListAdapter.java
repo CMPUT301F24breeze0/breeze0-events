ed3f30d0384b92950783a356075ae5efda831136
anyname-ui <shengku2@ualberta.ca>
2024-12-01 20:56:07 -0500

    sdf
    sdfsdf

--- app/src/main/java/com/example/breeze0events/AdminEventListAdapter.java
+++ app/src/main/java/com/example/breeze0events/AdminEventListAdapter.java
@@ -14,17 +14,36 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.List;
 
-
+/**
+ * The {@code AdminEventListAdapter} class is a custom {@link ArrayAdapter} for displaying a list of events in an admin interface.
+ * It populates each list item with event information, including the event name, start and end dates, and a poster image.
+ * <p>
+ * This adapter uses a custom layout defined in {@code admin_listview_with_image.xml}.
+ * If an event's poster image is unavailable or cannot be decrypted, a default placeholder image is used.
+ * </p>
+ */
 public class AdminEventListAdapter extends ArrayAdapter<Event> {
     private Context context;
     private List<Event> events;
-
+    /**
+     * Constructs a new {@code AdminEventListAdapter}.
+     *
+     * @param context The context in which the adapter will be used.
+     * @param events  The list of {@link Event} objects to display in the list.
+     */
     public AdminEventListAdapter(Context context, List<Event> events) {
         super(context, 0, events);
         this.context = context;
         this.events = events;
     }
-
+    /**
+     * Populates a view for a single item in the list with event details and a poster image.
+     *
+     * @param position    The position of the item within the adapter's data set.
+     * @param convertView The old view to reuse, if possible.
+     * @param parent      The parent view that this view will be attached to.
+     * @return A {@link View} corresponding to the data at the specified position.
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
